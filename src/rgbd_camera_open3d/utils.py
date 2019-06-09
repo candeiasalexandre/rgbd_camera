f52a56e2867a993463be4f6cef0ea656bdc1b193
candeiasalexandre <candeiasalexandre@gmail.com>
2019-06-09 01:13:31 +0100

    started developing a bridge between rgbd msg and open3d

---
+++ src/rgbd_camera_open3d/utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+import open3d as o3d
+
+from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+from rgbd_camera.msg import RgbdImage
+
+
+def rgbd_message_to_o3d(rgbd_image_msg):
+
+    pass 
+    return rgbd_image_o3d
